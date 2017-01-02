41339a789916fe670b143524dab1f30f02aac627
yvgenyk <yvgenykonrad@gmail.com>
2017-01-02 21:24:08 +0200

    imp5-card-view-update Did some styling to the text and calculation and layout change, GOLDEN RECTANGLE

--- app/src/main/java/com/example/yavengy/hidemyteam/adapter/ArticleAdapter.java
+++ app/src/main/java/com/example/yavengy/hidemyteam/adapter/ArticleAdapter.java
@@ -1,6 +1,8 @@
 package com.example.yavengy.hidemyteam.adapter;
 
 import android.content.Context;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -14,10 +16,14 @@ import com.example.yavengy.hidemyteam.model.Article;
 
 import java.util.List;
 
+import static com.example.yavengy.hidemyteam.activity.MainActivity.mainContext;
+import static com.example.yavengy.hidemyteam.activity.MainActivity.screenWidth;
+
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHolder> {
 
     private Context mContext;
     private List<Article> articleList;
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
@@ -26,7 +32,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHo
         public MyViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.title);
-            image = (TopCropImageView) view.findViewById(R.id.checkBox);
+            image = (TopCropImageView) view.findViewById(R.id.image);
         }
     }
 
@@ -48,6 +54,19 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHo
         Article article = articleList.get(position);
         holder.title.setText(article.getTitle());
         holder.image.setImageBitmap(article.getImage());
+
+        int width = screenWidth - (int) mainContext.getResources().getDimension(R.dimen.article_margin)*2;
+
+        int totalHeight = (int) (width/1.618);
+        int imageHeight = (int) (totalHeight*0.8);
+        int textHeight = totalHeight - imageHeight;
+
+        holder.title.getLayoutParams().height = textHeight;
+        holder.image.getLayoutParams().height = imageHeight;
+
+        Log.i("title", String.valueOf(textHeight));
+        Log.i("image", String.valueOf(imageHeight));
+        Log.i("width", String.valueOf(width));
     }
 
     @Override
